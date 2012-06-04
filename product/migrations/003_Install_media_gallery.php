c690c548fb39908387afc050f5869ebb6567ac9a
tricinel <bogdan@blazardesign.com>
2012-06-04 11:18:31 +0300

    modified database structure

--- product/migrations/003_Install_media_gallery.php
+++ product/migrations/003_Install_media_gallery.php
@@ -9,9 +9,10 @@ class Migration_Install_media_gallery extends Migration {
 		$this->dbforge->add_field('`id` int(11) NOT NULL AUTO_INCREMENT');
 			$this->dbforge->add_field("`product_sku` VARCHAR(64) NOT NULL");
 			$this->dbforge->add_field("`image_path` VARCHAR(250) NOT NULL");
-			$this->dbforge->add_field("`image_position` TINYINT(1) NOT NULL");
-			$this->dbforge->add_field("`image_is_default` TINYINT(1) NOT NULL");
-			$this->dbforge->add_field("`image_type` ENUM('thumb','small_image','large_image') NOT NULL");
+			$this->dbforge->add_field("`image_position` TINYINT(1) NOT NULL DEFAULT '0'");
+			$this->dbforge->add_field("`image_is_default` TINYINT(1) NOT NULL DEFAULT '0'");
+			$this->dbforge->add_field("`image_is_thumb` TINYINT(1) NOT NULL DEFAULT '0'");
+			$this->dbforge->add_field("`image_is_small_image` TINYINT(1) NOT NULL DEFAULT '0'");
 			$this->dbforge->add_field("`image_label` VARCHAR(128) NULL");
 		$this->dbforge->add_key('id', true);
 		$this->dbforge->create_table('product_media');
--- product/migrations/004_Install_stock.php
+++ product/migrations/004_Install_stock.php
@@ -8,8 +8,8 @@ class Migration_Install_stock extends Migration {
 
 		$this->dbforge->add_field('`id` int(11) NOT NULL AUTO_INCREMENT');
 			$this->dbforge->add_field("`product_sku` VARCHAR(64) NOT NULL");
-			$this->dbforge->add_field("`qty` INT(11) NOT NULL");
-			$this->dbforge->add_field("`low_stock_qty` INT(11) NOT NULL");
+			$this->dbforge->add_field("`qty` INT(11) NULL");
+			$this->dbforge->add_field("`low_stock_qty` INT(11) NULL");
 			$this->dbforge->add_field("`is_in_stock` TINYINT(1) NOT NULL");
 			$this->dbforge->add_field("`manage_stock` TINYINT(1) NOT NULL");
 		$this->dbforge->add_key('id', true);
